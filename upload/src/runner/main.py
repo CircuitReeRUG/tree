eb12b8f4ae7488c50bf84f426ba7707194033a9f
Boyan <boyan@confest.im>
2025-12-14 00:37:58 +0100

    FAWK

--- upload/src/runner/main.py
+++ upload/src/runner/main.py
@@ -1,7 +1,7 @@
 import sys
 from .color import Color
 from RestrictedPython import compile_restricted
-from RestrictedPython.Guards import safe_builtins, safer_getattr, guarded_iter_unpack_sequence
+from RestrictedPython.Guards import safe_builtins, safer_getattr, guarded_iter_unpack_sequence, guarded_unpack_sequence
 from RestrictedPython.Eval import default_guarded_getiter
 from RestrictedPython.PrintCollector import PrintCollector
 import math
@@ -11,20 +11,18 @@ from .exposed import get_exposed_functions
 def _inplacevar_(op, var, expr):
     ops = {"+=": lambda: var + expr, "-=": lambda: var - expr, "*=": lambda: var * expr,
            "/=": lambda: var / expr, "%=": lambda: var % expr, "**=": lambda: var ** expr,
-           "<<=": lambda: var << expr, ">>=": lambda: var >> expr, "|=": lambda: var | expr,
+           "<<": lambda: var << expr, ">>=": lambda: var >> expr, "|=": lambda: var | expr,
            "^=": lambda: var ^ expr, "&=": lambda: var & expr, "//=": lambda: var // expr}
     return ops.get(op, lambda: var)()
 
 def execute_code(code):
     byte_code = compile_restricted(code, "<user_code>", "exec")
-    
     allowed_builtins = {**safe_builtins, "enumerate": enumerate, "zip": zip, "map": map,
                         "filter": filter, "sorted": sorted, "reversed": reversed, "sum": sum,
                         "min": min, "max": max, "abs": abs, "round": round, "pow": pow,
                         "divmod": divmod, "all": all, "any": any, "isinstance": isinstance,
                         "chr": chr, "ord": ord, "hex": hex, "oct": oct, "bin": bin,
                         "range": range}
-    
     restricted_globals = {
         "__builtins__": allowed_builtins,
         "_getitem_": lambda obj, index: obj[index],
@@ -33,7 +31,7 @@ def execute_code(code):
         "_getattr_": safer_getattr,
         "_getiter_": default_guarded_getiter,
         "_iter_unpack_sequence_": guarded_iter_unpack_sequence,
-        "_unpack_sequence_": guarded_iter_unpack_sequence,
+        "_unpack_sequence_": guarded_unpack_sequence,
         "__name__": "restricted_module",
         "__metaclass__": type,
         "RED": Color.RED, "GREEN": Color.GREEN, "BLUE": Color.BLUE,
@@ -42,7 +40,6 @@ def execute_code(code):
         "PINK": Color.PINK, "OFF": Color.OFF,
         "math": math, "random": random
     }
-    
     restricted_globals.update(get_exposed_functions())
     
     try:
